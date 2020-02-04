a3aa80eb79814467cf9f0ad045732c65bd8b8caf
Petter Strandh <Petter.strandh@volvo.com>
2020-02-04 10:21:01 +0100

    First add

---
+++ utils.h
@@ -0,0 +1,23 @@
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <stdint.h>
+
+enum
+{
+	LEFT_OUTHER_SENSOR,
+	LEFT_INNER_SENSOR,
+	MID_SENSOR,
+	RIGHT_INNER_SENSOR,
+	RIGHT_OUTHER_SENSOR,
+};
+
+#define TRUE (0 == 0)
+#define FALSE (0 != 0)
+
+void play_beep(void);
+void stop(void);
+int16_t limit(int16_t in, int16_t min, int16_t max);
+
+#endif /* UTILS_H_ */
