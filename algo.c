0bb6a98340a7b13189078bc5668efdd7e400c730
Petter Strandh <Petter.Strandh@volvo.com>
2020-02-05 12:17:48 +0100

    working on algo

--- algo.c
+++ algo.c
@@ -3,10 +3,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
-const uint8_t x_size = 4;
-const uint8_t y_size = 5;
-const uint8_t node_size = x_size*2 + y_size*2;
-uint8_t matris[4][5];
+#define x_size 4
+#define y_size 3
+#define NODE_SIZE 15 // (x_size*2 + y_size*2)
+
+struct node_coordinate
+{
+    uint8_t xk;
+    uint8_t yk;
+};
+
+struct node_coordinate nodes[NODE_SIZE];
 
 enum dir {_low=2, _east=3, _south=4, _west=5, _north=6, _high=7, _higher=8};
 
@@ -41,28 +48,88 @@ uint8_t turn_180 (enum dir direction)
     direction = direction + 2;
     if (direction == _high)
         direction = _east;
-        else if(direction == _higher)
+    else if(direction == _higher)
         {
             direction = _south;
         }
-        
+
     return direction;
 }
 
 
 void pass_line(uint8_t direction, uint8_t * x1, uint8_t * y1)
 {
-    switch (direction) 
-    {
-        case _east :  *y1 = *y1+1; break;
-        case _south : *x1 = *x1+1; break;
-        case _west :  *y1 = *y1-1; break;
-        case _north : *x1 = *x1-1; break;
-        default : break;
-    }
-
+    switch (direction)
+        {
+        case _east :
+            *y1 = *y1+1;
+            break;
+        case _south :
+            *x1 = *x1+1;
+            break;
+        case _west :
+            *y1 = *y1-1;
+            break;
+        case _north :
+            *x1 = *x1-1;
+            break;
+        default :
+            break;
+        }
     return;
 }
+
+
+void handle_nodes ()
+{
+
+
+}
+
+
+void init(struct node_coordinate nodes[])
+{
+   uint8_t counter;
+   uint8_t init_x = 0;
+   uint8_t init_y = 1;
+   
+   for(counter = 0; counter<NODE_SIZE; counter++)
+   {
+        nodes[counter].xk = 0xEE;
+        nodes[counter].yk = 0xEF;
+   }
+   
+  
+  //Go East.
+    for(counter = 1; counter < y_size+1; counter++)
+    {
+        nodes[counter].xk = init_x;
+        nodes[counter].yk = init_y;
+        init_y++;
+    }
+    //Go south
+    for(counter = y_size+1; counter < y_size+x_size+1; counter++)
+    {
+        nodes[counter].xk = init_x;
+        nodes[counter].yk = init_y;
+        init_x++;
+    }
+    //Go west
+    for(counter = y_size+x_size+1; counter < y_size+x_size+y_size+1; counter++)
+    {
+        nodes[counter].xk = init_x;
+        nodes[counter].yk = init_y;
+        init_y--;
+    }
+    //Go north
+    for(counter = y_size+x_size+y_size+1; counter <y_size+x_size+y_size+x_size+1; counter++)
+    {
+        nodes[counter].xk = init_x;
+        nodes[counter].yk = init_y;
+        init_x--;
+    }
+}
+
 
 
 #ifdef MAIN
@@ -72,7 +139,7 @@ int main()
     uint8_t a = 3;
     uint8_t b = turn_left(a);
     printf("turn_left %d, %d\n", a, b);
-    
+
     a=6;
     uint8_t c = turn_right(a);
     printf("turn_right %d, %d\n", a, c);
@@ -88,9 +155,13 @@ int main()
     pass_line(_north, &x1, &y1);
     printf(", pass_line %d, %d\n", x1, y1);
 
+    init(nodes);
 
+    uint8_t counter;
+    for(counter=1; counter < NODE_SIZE; counter++ )
+        printf("N:%d \t x: %d\t y: %d\n",counter,nodes[counter].xk, nodes[counter].yk);
 
     return 0;
 }
 
-#endif
+#endif // end of Main
