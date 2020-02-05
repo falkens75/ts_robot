b332b000f638834421e26ca8b9bf08c7fe252525
Petter Strandh <Petter.Strandh@volvo.com>
2020-02-05 08:20:41 +0100

    working on algo

---
+++ algo.c
@@ -0,0 +1,41 @@
+//gcc algo.c -o algo -Wall -pedantic -DMAIN
+
+#include <stdio.h>
+#include <stdint.h>
+
+const uint8_t x_size = 4;
+const uint8_t y_size = 5;
+const uint8_t node_size = x_size*2 + y_size*2;
+uint8_t matris[4][5];
+
+
+const uint8_t east = 3;
+const uint8_t south = 4;
+const uint8_t west = 5;
+const uint8_t north = 6;
+
+uint8_t turn_left (uint8_t direction)
+{
+    direction = direction-1;
+    if(direction == 2)
+        direction = 6;
+
+    return direction;
+}
+
+
+
+
+#ifdef MAIN
+int main()
+{
+
+    uint8_t a = 3;
+    uint8_t b = turn_left(a);
+
+    printf(" %d, %d ", a, b);
+    
+    return 0;
+}
+
+#endif
